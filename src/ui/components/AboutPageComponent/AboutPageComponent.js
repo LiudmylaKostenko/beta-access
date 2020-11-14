37533970fae13943aa04882074bb7876a4b69108
Ludmila2603 <mika22105@gmail.com>
2020-11-14 13:22:47 +0300

    added component displaying page about company

---
+++ src/ui/components/AboutPageComponent/AboutPageComponent.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const AboutPageComponent = () => (
+	<div className="container p-2">
+		<div className="row">
+			<div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 p-2 d-flex justify-content-center">
+				<h1>Who we are?</h1>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+				<p className="text-justify p-2">
+					We are a group of like-minded people who are engaged in the protection and assistance of homeless
+					animals, as well as animals that have either suffered at the hands of humans, or are lost, or are in
+					trouble. All the pets of our shelter really need help! Every day they need food, veterinary care
+					(regular treatment for fleas, worms and other dangerous parasites, as well as vaccinations, planned
+					and unscheduled treatment of various diseases), warmth and a roof over their heads.
+				</p>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+				<p className="text-justify pl-2 pr-2">
+					The help of each of you will give the opportunity to make the life of these animals better, as well
+					as find them owners, which the animals are waiting for!
+				</p>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 p-2 d-flex justify-content-center">
+				<p className="text-justify pl-2 pr-2">Please take part in solving this problem!</p>
+			</div>
+		</div>
+	</div>
+);
+
+export default AboutPageComponent;
